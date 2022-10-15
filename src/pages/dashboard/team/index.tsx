fb43713224832b1ac8fb31d02d6847fd4131b97e
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-10-15 18:44:10 -0300

    modal to add members to rper

--- src/pages/dashboard/team/index.tsx
+++ src/pages/dashboard/team/index.tsx
@@ -1,5 +1,7 @@
+import { useCallback, useRef } from 'react'
 import { RiExchangeFill } from 'react-icons/ri'
 import { IoMdAdd } from 'react-icons/io'
+import AddTeamModal from './modal-add-team'
 import {
   Container,
   TeamContainer,
@@ -8,6 +10,12 @@ import {
 } from './styles'
 
 const Team: React.FC = () => {
+  const modalRef = useRef<HTMLDialogElement>(null)
+
+  const openModal = useCallback(() => {
+    modalRef.current?.showModal()
+  }, [modalRef])
+
   return (
     <Container>
       <h2>
@@ -17,7 +25,7 @@ const Team: React.FC = () => {
       <TeamContainer>
         <TeamHeader>
           <h3>Team</h3>
-          <button>
+          <button onClick={openModal}>
             <IoMdAdd />
           </button>
         </TeamHeader>
@@ -30,7 +38,63 @@ const Team: React.FC = () => {
           </div>
           <strong>Name Surname (Coordinator)</strong>
         </article>
+        <article>
+          <div>
+            <PlaceholderLoading>
+              <div></div>
+              <div></div>
+            </PlaceholderLoading>
+          </div>
+          <strong>Name Surname</strong>
+        </article>
+        <article>
+          <div>
+            <PlaceholderLoading>
+              <div></div>
+              <div></div>
+            </PlaceholderLoading>
+          </div>
+          <strong>Name Surname</strong>
+        </article>
+        <article>
+          <div>
+            <PlaceholderLoading>
+              <div></div>
+              <div></div>
+            </PlaceholderLoading>
+          </div>
+          <strong>Name Surname</strong>
+        </article>
+        <article>
+          <div>
+            <PlaceholderLoading>
+              <div></div>
+              <div></div>
+            </PlaceholderLoading>
+          </div>
+          <strong>Name Surname</strong>
+        </article>
+        <article>
+          <div>
+            <PlaceholderLoading>
+              <div></div>
+              <div></div>
+            </PlaceholderLoading>
+          </div>
+          <strong>Name Surname</strong>
+        </article>
+        <article>
+          <div>
+            <PlaceholderLoading>
+              <div></div>
+              <div></div>
+            </PlaceholderLoading>
+          </div>
+          <strong>Name Surname</strong>
+        </article>
       </TeamContainer>
+
+      <AddTeamModal ref={modalRef} />
     </Container>
   )
 }
